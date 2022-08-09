3f33ca6524d1513fd583223954a557354d04518f
broliver18 <bruno.olive504@gmail.com>
2022-08-09 18:42:42 -0400

    Connect methods and add basic functionality

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 import './Playlist.css';
 
+import TrackList from '../TrackList/TrackList';
+
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(e) {
+        let newName = e.target.value;
+        this.props.onNameChange(newName);
+    }
+
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'}/>
-                {/* <TrackList /> */}
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
+                <TrackList tracks={this.props.playlistTracks} 
+                    onRemove={this.props.onRemove}
+                    isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
